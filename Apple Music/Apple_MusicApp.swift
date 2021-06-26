f701ba2a71491ebac80acd6c4905103777587326
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2021-06-26 09:13:26 +0800

    Initial Commit

---
+++ Apple Music/Apple_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_MusicApp.swift
+//  Apple Music
+//
+//  Created by Runhua Huang on 2021/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Apple_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Music/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple Music
+//
+//  Created by Runhua Huang on 2021/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
